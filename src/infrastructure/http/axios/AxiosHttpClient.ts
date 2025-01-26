0265afea1aee09d68ea0c2457a586cdde1ff6d4b
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2025-01-26 13:53:42 -0300

    fix: header Authorization to req api

--- src/infrastructure/http/axios/AxiosHttpClient.ts
+++ src/infrastructure/http/axios/AxiosHttpClient.ts
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+const token = localStorage.getItem('token')
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
+    'Authorization': `Bearer ${token}`,
     'Content-type': 'application/json'
   }
 })
