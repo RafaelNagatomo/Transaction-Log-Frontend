8042d31a730e295fcb8bdc8e8ab5ebf862ddb35f
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2025-01-27 12:55:47 -0300

    feat: transaction infra repository created

---
+++ src/infrastructure/repositories/TransactionRepositoryImpl.ts
@@ -0,0 +1,57 @@
+import api from '../http/axios/AxiosHttpClient'
+import Transaction from '~/domain/entities/Transaction'
+import ITransactionRepository from '~/domain/repositories/ITransactionRepository'
+
+
+export default class TransactionRepositoryImpl implements ITransactionRepository {
+  async createTransaction(transaction: Transaction): Promise<Transaction> {
+    const response = await api.post('/transactions/create', {
+      description: transaction.description || '',
+      amount: transaction.amount || 0,
+      type: transaction.type || 'income',
+      status: transaction.status || 'pending',
+      isActive: transaction.isActive || false
+    })
+
+    const newTransaction: Transaction = response.data
+
+    return newTransaction
+  }
+
+  async findAllTransactions(): Promise<Transaction[]> {
+    const allTransactions = await api.get('/transactions/', {})
+    return allTransactions.data
+  }
+
+  async findTransactionById(id: string): Promise<Transaction | null> {
+    await api.get(`/transactions/find/?id=${id}`)
+
+    return {
+      id,
+      type: 'income',
+      amount: 0,
+      description: '',
+      status: 'pending',
+      isActive: false
+    }
+  }
+
+  async updateTransaction(transaction: Transaction): Promise<Transaction | null> {
+    await api.put(`/transactions/update/?id=${transaction.id}`, transaction)
+
+    return transaction
+  }
+
+  async deleteTransaction(id: string): Promise<Transaction | null> {
+    await api.put(`/transactions/delete/?id=${id}`, id)
+
+    return {
+      id,
+      type: 'income',
+      amount: 0,
+      description: '',
+      status: 'pending',
+      isActive: false
+    }
+  }
+}
