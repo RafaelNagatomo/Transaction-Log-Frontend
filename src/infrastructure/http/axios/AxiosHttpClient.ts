edea425d5562061b85cc94333cc36883cee0043d
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2025-01-30 09:11:27 -0300

    fix: token localStorage name

--- src/infrastructure/http/axios/AxiosHttpClient.ts
+++ src/infrastructure/http/axios/AxiosHttpClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const token = localStorage.getItem('token')
+const token = localStorage.getItem('authToken')
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
